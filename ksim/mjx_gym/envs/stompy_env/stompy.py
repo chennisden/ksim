f28b2a891d68c427f55e387c7cd808c3b7ada10a
Nathan Zhao <nathanzh@stanford.edu>
2024-07-16 22:02:58 -0700

    Added streaming on mujoco with mjx implemenation
    FPS is very low as MJX renderer is slow. Replacing with Mujoco renderer
    breaks standing policy though.

--- ksim/mjx_gym/envs/stompy_env/stompy.py
+++ ksim/mjx_gym/envs/stompy_env/stompy.py
@@ -30,8 +30,7 @@ class StompyEnv(PipelineEnv):
         log_reward_breakdown: bool = True,
         **kwargs: Unpack[EnvKwargs],
     ) -> None:
-        # path = os.getenv("MODEL_DIR", "") + "/stompy/stompy_armless.xml"
-        path = "/home/awu/ksim/ksim/assets/inertia_legs/legs.xml"
+        path = os.getenv("MODEL_DIR", "") + "/assets/inertia_legs/legs.xml"
         mj_model = mujoco.MjModel.from_xml_path(path)
         mj_model.opt.solver = mujoco.mjtSolver.mjSOL_CG
         mj_model.opt.iterations = 6
